c1ba3dc191a58150ec079748b1c29c73200cea1b
johnhopkins <john@johnhopkins.co.uk>
2016-10-12 15:15:01 +0100

    Added basic audio and EVS objects

---
+++ EVS.py
@@ -0,0 +1,9 @@
+class Evs(object):
+
+    def __init__(self, evsNumber, numberOfInputs, numberOfOutputs):
+        self.evsNumber = evsNumber
+        self.inputs = numberOfInputs
+        self.outputs = numberOfOutputs
+
+    def getEvsNumber(self):
+        return self.getEvsNumber
---
+++ audio.py
@@ -0,0 +1,23 @@
+class Audio(object):
+
+    def __init__ (a,b,c,d,e,f,g,h,i,j,k,m,n,o,p):
+       self.a = a
+       self.b = b
+       self.c = c
+       self.d = d
+       self.e = e
+       self.f = f
+       self.g = g
+       self.h = h
+       self.a = i
+       self.b = j
+       self.c = k
+       self.d = l
+       self.e = m
+       self.f = n
+       self.g = o
+       self.h = p
+
+    def getAllAudios(self):
+        return self.a, self.b, self.c, self.d, self.e, self.f, self.g, self.h, self.i, self.j, self.k, self.l, self.m, self.n, self.o, self.p
+
