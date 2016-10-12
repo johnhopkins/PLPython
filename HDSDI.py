27d924a371c74502933c36a40a2244f42e4f77a1
johnhopkins <john@johnhopkins.co.uk>
2016-10-12 15:17:52 +0100

    basic clean up

--- HDSDI.py
+++ HDSDI.py
@@ -5,22 +5,3 @@ class HDSDI(object):
 
     def getVision(self):
         return self.vision
-
-    def getAvailableExternals(self):
-        '''Returns a sorted list of gallery externals that are not set to False, i.e. they are available'''
-        availableExternals = []
-        for external, value in self.externals.iteritems():
-            if value == False:
-                availableExternals.append(external)
-        return sorted(availableExternals)
-
-    def getAllTxs(self):
-        return self.tx
-
-    def getAvailableTxs(self):
-        '''Returns a sorted list of gallery txs that are not set to False, i.e. they are available'''
-        availableTxs = []
-        for tx, value in self.tx.iteritems():
-            if value == False:
-                availableTxs.append(tx)
-        return sorted(availableTxs)
