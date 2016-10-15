4597f3ace729edbbd482cf1ebcc6dfeaab0478bb
johnhopkins <john@johnhopkins.co.uk>
2016-10-15 18:47:57 +0100

    Added basic vision class

--- HDSDI.py
+++ HDSDI.py
@@ -1,7 +1,17 @@
 class HDSDI(object):
-    def __init__(self):
-        self.vision = {'Vision': True}
-        self.audios = {'Audio 1': False, 'Audio 2':False}
+    '''This class is essentially a wrapper that holds a vision object and an audio object'''
+    def __init__(self, vision, audios):
+        self.vision = vision
+        self.audios = audios
 
     def getVision(self):
         return self.vision
+
+    def getAudios(self):
+        return self.audios
+
+    def setAudios(self, audios):
+        self.audios = audios
+
+    def setVision(self, vision):
+        self.vision = vision
