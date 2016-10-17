3a45b350eccdf5c6859a4b0213d60e4e83ce0d03
johnhopkins <johnhopkins@gmx.co.uk>
2016-10-17 17:34:00 +0100

    Progress with gui and main run loop Added date setter method to matchday.py

--- gui.py
+++ gui.py
@@ -1,13 +1,44 @@
-from swampy.Gui import *
+from tkinter import *
+from matchday import *
+import datetime
 
-g = Gui()
-g.title('PLP Routing')
+myMatchday = MatchDay()
 
-listOfClubs = ['Liverpool','Man United', 'Tottenham', 'Man City', 'Everton', 'Arsenal', 'West Ham', 'Sunderland', 'Stoke', 'Crystal Palace', 'Southampton', 'Burnley', 'Hull', 'Watford', 'West Brom', 'Leicester', 'Chelsea','Bournemouth','Swansea','Middlesbrough']
+win = Tk()
 
-listOfClubs.sort()
+win.title('PL Routing')
 
-for each in listOfClubs:
-    button = g.bu(text=each)
+label = Label(text = 'Input the date')
+day = Label(text = 'Day')
 
-g.mainloop()
+variable1 = StringVar(win)
+variable1.set(1)
+variable2 = StringVar(win)
+variable2.set(1)
+variable3 = StringVar(win)
+variable3.set(2016)
+
+selectedDay = OptionMenu(win, variable1, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31)
+month = Label(text ='Month')
+selectedMonth = OptionMenu(win, variable2, 1,2,3,4,5,6,7,8,9,10,11,12)
+year = Label(text = 'Year')
+selectedYear = OptionMenu(win, variable3, 2016,2017,2018,2019,2020,2021)
+
+label.grid(row = 0, column = 0)
+day.grid(row = 1, column = 0)
+month.grid(row = 1, column = 2)
+year.grid(row = 1, column = 4)
+selectedDay.grid(row=1, column = 1)
+selectedMonth.grid(row=1, column = 3)
+selectedYear.grid(row=1, column = 5)
+
+def ok():
+
+    mydate = datetime.date(int(variable3.get()), int(variable2.get()), int(variable1.get()))
+    myMatchday.setDate(mydate)
+    print(mydate)
+
+button = Button(win, text="OK", command=ok)
+button.grid(row = 2, column = 3)
+
+win.mainloop()
--- matchday.py
+++ matchday.py
@@ -1,10 +1,13 @@
 class MatchDay(object):
-    def __init__(self, date):
-        self.date = date
+    def __init__(self):
+        self.date = 0
         self.matches = []
 
     def getDate(self):
         return self.date
+
+    def setDate(self, date):
+        self.date = date
 
     def addMatch(self, matchObject):
         self.matches.append(matchObject)
