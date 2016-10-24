21f5ac4f6f60f6cdd682e787483e5f5cdac31be8
johnhopkins <johnhopkins@gmx.co.uk>
2016-10-24 21:21:01 +0100

    Fixtures.py and matchpicker.py added. Improvements to datepanel.py

---
+++ matchpicker.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class MatchPicker(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+        self.teams = ['Crystal Palace','Leicester','Middlesbrough','Bournemouth','Southampton','Sunderland','Stoke','Liverpool','West Brom','Arsenal','Tottenham','West Ham','Watford','Everton','Chelsea','Man United','Man City','Hull City','Burnley','Swansea']
+        self.a = 2
+        self.teams.sort()
+        for team in self.teams:
+            self.button = Button(text=team, width=12)
+            self.button.grid(row=self.a, column=1)
+            self.a += 1
--- tkDatePanel.py
+++ tkDatePanel.py
@@ -4,30 +4,26 @@ from datetime import *
 class DatePanel(Frame):
     '''A basic date selector widget that can be used in any tkinter project.
     Requires tkinter (obviously) and datetime modules.'''
-    def __init__(self, parent, **options):
-        Frame.__init__(self, parent, **options)
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
         self.dayLabel = Label(self, text="Day")
         self.monthLabel = Label(self, text="Month")
         self.yearLabel = Label(self, text="Year")
-        self.dayLabel.grid(row=0, column=0)
-        self.monthLabel.grid(row=0, column=1)
-        self.yearLabel.grid(row=0, column=2)
+        self.dayLabel.grid(row=0, column=0, sticky=E+W)
+        self.monthLabel.grid(row=0, column=1, sticky=E+W)
+        self.yearLabel.grid(row=0, column=2, sticky=E+W)
         optionlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
         self.variable1 = IntVar()
         self.variable1.set(datetime.today().day)
         self.dropMenu1 = OptionMenu(self, self.variable1, *optionlist)
-        self.dropMenu1.config(bg='RED')
         self.dropMenu1.grid(row=1, column=0)
         optionlist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         self.variable2 = IntVar()
         self.variable2.set(datetime.today().month)
         self.dropMenu2 = OptionMenu(self, self.variable2, *optionlist)
-        self.dropMenu2.config(bg='RED')
         self.dropMenu2.grid(row=1, column=1)
         optionlist = [2016, 2017, 2018, 2019, 2020, 2021]
         self.variable3 = IntVar()
         self.variable3.set(datetime.today().year)
         self.dropMenu3 = OptionMenu(self, self.variable3, *optionlist)
-        self.dropMenu3.config(bg='RED')
-        self.dropMenu3['menu'].config(bg='RED')
         self.dropMenu3.grid(row=1, column=2)
