0b91e2febd264a469cd404e874bb773ce27b34d5
johnhopkins <john@johnhopkins.co.uk>
2016-10-27 12:29:29 +0100

    General improvements to application class

--- gui.py
+++ gui.py
@@ -11,10 +11,11 @@ class Application(Frame):
 
         date = datetime(self.datepanel.variable3.get(), self.datepanel.variable2.get(), self.datepanel.variable1.get()).date()
         self.myMatchDay.setDate(date)
-        self.label.grid_forget()
-        self.label = Label(self.frame1)
-        self.label['text'] = str(self.myMatchDay.date.strftime('%d %B %Y'))
-        self.label.grid(row=3, column=3)
+        self.label1.grid_forget()
+        self.label1 = Label(self.frame1)
+        self.label1['text'] = str(self.myMatchDay.date.strftime('%d %B %Y'))
+        self.label1.grid(row=3, column=3)
+
 
     def createwidgets(self):
 
@@ -26,15 +27,16 @@ class Application(Frame):
         self.datepanel.grid(row=1, column=0, columnspan=3)
         self.ok = Button(self.frame1, text='Set Date', command=self.gotomatchday)
         self.ok.grid(row=3, column=1)
+        self.label1 = Label(self.frame1)
+        self.label1['text'] = str(self.myMatchDay.date.strftime('%d %B %Y'))
+        self.label1.grid(row=3, column=3)
         self.frame1.grid()
+        print(self.myMatchDay.getDate())
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.myMatchDay = MatchDay()
         self.grid()
-        self.variable1 = 1
-        self.variable2 = 2
-        self.variable3 = 2012
         self.createwidgets()
 
 root = Tk()
