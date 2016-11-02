05697f68f177d2134ba12b4657f7512e5a2eb671
John Hopkins <johnhopkins@gmx.co.uk>
2016-11-02 23:24:21 +0000

    Update matchpicker.py
    Fixed lamda button function via github

--- matchpicker.py
+++ matchpicker.py
@@ -8,5 +8,5 @@ class MatchPicker(tk.Frame):
         self.teams = ['Crystal Palace','Leicester','Middlesbrough','Bournemouth','Southampton','Sunderland','Stoke','Liverpool','West Brom','Arsenal','Tottenham','West Ham','Watford','Everton','Chelsea','Man United','Man City','Hull City','Burnley','Swansea']
         self.teams.sort()
         for team in self.teams:
-            self.button = Button(text=team, width=12, command=lambda: Fixtures.test(self.teams))
+            self.button = Button(text=team, width=12, command=lambda team=team: Fixtures.test(team))
             self.button.pack()
