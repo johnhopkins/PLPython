f806b4559053fe9487552efe6ad928d42d1d6b45
John Hopkins <johnhopkins@gmx.co.uk>
2016-11-02 23:27:47 +0000

    Update matchpicker.py
    Altered import directive

--- matchpicker.py
+++ matchpicker.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from fixtures import *
+import fixtures as fix
 
 
 class MatchPicker(tk.Frame):
@@ -8,5 +8,5 @@ class MatchPicker(tk.Frame):
         self.teams = ['Crystal Palace','Leicester','Middlesbrough','Bournemouth','Southampton','Sunderland','Stoke','Liverpool','West Brom','Arsenal','Tottenham','West Ham','Watford','Everton','Chelsea','Man United','Man City','Hull City','Burnley','Swansea']
         self.teams.sort()
         for team in self.teams:
-            self.button = Button(text=team, width=12, command=lambda team=team: Fixtures.test(team))
+            self.button = Button(text=team, width=12, command=lambda team=team: fix.Fixtures.test(team))
             self.button.pack()
